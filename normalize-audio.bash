2502f2211b09cf8f981c3ee317ef48bdebdb930c
Mikael Roos <mikael.t.h.roos@gmail.com>
2016-03-09 10:34:02 +0100

    allow files in subdir when normalize audio

--- normalize-audio.bash
+++ normalize-audio.bash
@@ -3,21 +3,28 @@
 # Normalize tha audio to 0 dB when detecting negative dB values.
 #
 
+src=${1:-aud.wav}
 
-echo -n "Analyzing..."
-db=$( ffmpeg -i aud.wav -af "volumedetect" -f null /dev/null 2>&1 | grep max_volume | awk -F': ' '{print $2}' | cut -d' ' -f1 )
+dir=$(dirname "$src")
+filename=$(basename "$src")
+extension="${filename##*.}"
+filename="${filename%.*}"
+target="$dir/${filename}_norm.$extension"
+
+echo -n "Analyzing src=''$src'"
+db=$( ffmpeg -i "$src" -af "volumedetect" -f null /dev/null 2>&1 | grep max_volume | awk -F': ' '{print $2}' | cut -d' ' -f1 )
 echo " detected $db dB"
 
 if [ "$( echo "$db < 0" | bc )" -eq 1 ]; then
-    echo "Normalising using ${db#?}dB..."
-    ffmpeg -i aud.wav -af "volume=${db#?}dB" -c:v copy aud_normalized.wav -loglevel quiet
-    cp aud.wav aud_original.wav
-    cp aud_normalized.wav aud.wav
-    
-    echo -n "Analyzing..."
-    db=$( ffmpeg -i aud.wav -af "volumedetect" -f null /dev/null 2>&1 | grep max_volume | awk -F': ' '{print $2}' | cut -d' ' -f1 )
-    echo " detected $db dB"
+    echo "Normalising '$src' using ${db#?}dB..."
+    ffmpeg -i "$src" -af "volume=${db#?}dB" -c:v copy "$target" -loglevel quiet
 
+    echo -n "Analyzing result='$target'"
+    db=$( ffmpeg -i "$target" -af "volumedetect" -f null /dev/null 2>&1 | grep max_volume | awk -F': ' '{print $2}' | cut -d' ' -f1 )
+    echo " detected $db dB"
+    ls -l "$src" "$target"
+    echo vlc \"$src\"
+    echo vlc \"$target\"
 else
     echo "Ignore normalising."
 fi
