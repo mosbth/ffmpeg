138bb4f6a8644f6af2da99f07b615ce2476e223a
Mikael Roos <mikael.t.h.roos@gmail.com>
2016-09-16 09:22:21 +0200

    modified on bth

--- delay_audio.sh
+++ delay_audio.sh
@@ -8,5 +8,5 @@ extension="${filename##*.}"
 filename="${filename%.*}"
 OUTPUT="$dir/${filename}_synca$DELAY.$extension"
 
-ffmpeg -i $INPUT -itsoffset $DELAY -i $INPUT -vcodec copy -acodec copy -map 0:0 -map 1:1 $OUTPUT
-vlc $OUTPUT
+ffmpeg -i "$INPUT" -itsoffset $DELAY -i "$INPUT" -vcodec copy -acodec copy -map 0:0 -map 1:1 "$OUTPUT"
+vlc "$OUTPUT"
